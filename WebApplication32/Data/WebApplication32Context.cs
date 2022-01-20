fe9b063f6ba8a9825b7706399a4a942b6c053c39
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-20 18:05:07 +0530

    Add project files.

---
+++ WebApplication32/Data/WebApplication32Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication32.Model;
+
+namespace WebApplication32.Data
+{
+    public class WebApplication32Context : DbContext
+    {
+        public WebApplication32Context (DbContextOptions<WebApplication32Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication32.Model.Customer> Customer { get; set; }
+    }
+}
